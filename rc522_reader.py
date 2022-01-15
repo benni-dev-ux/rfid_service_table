381603d38c67181029e954a86a45edab57eebf35
Benedikt <benediktsteinmueller@googlemail.com>
2022-01-15 19:21:25 +0100

    cleanup r522_reader.py

--- rc522_reader.py
+++ rc522_reader.py
@@ -22,42 +22,38 @@ yellow_button = 6
 
 
 def power_button():
-    print("\n Shutting down the Device")
+    print("Shutting down the Device")
     check_call(['sudo', 'poweroff'])
 
 
 def pause_button():
-    print("\n Pause button pressed")
+    print("Play/Puse")
 
     try:
         player
     except NameError:
-        print("\n No music player defined")
+        print("No music player defined")
     else:
         player.play_pause()
 
 
 def back_button():
-    print("\n Stopping all Media")
+    print("Stopping all Media")
     media_player.stop_all_media()
 
 
 def forward_button():
-    print("\n forward button pressed")
-
-    toggle_display(True)
+    print("forward button pressed")
+    if SCREEN_TURN_OFF:
+        toggle_display(True)
 
 
 def main():
-    # if START_UP_SOUND:
-    #     startup = pygame.mixer.Sound(
-    #         "/home/pi/Raspi_RFID_player/assets/startup.wav")
-    #     startup.play()
-    #media_player.play_feedback_sound()
+    if START_UP_SOUND:
+        media_player.play_feedback_sound()
     print("\n RFID Player Ready")
 
     global player
-
 
     button1 = Button(black_button, hold_time=2)
     button2 = Button(yellow_button, bounce_time=0.1)
@@ -72,62 +68,47 @@ def main():
 
     reader = SimpleMFRC522()
 
-    lastcode = [-1,-1,-1,-1,-1]
-    last=-1
+    last_codes_lst = [-1, -1, -1, -1, -1]
+    last_media_code = -1
     is_paused = False
 
     # Main Loop of the App: Constantly checking for new  RFID input
     while True:
-        #print("reading")
         code = read_tag(reader)
-        #print(code)
-        
-        lastcode.insert(0,code)
-        lastcode.pop()
-        #print(lastcode)
-        
-        comp = sum(lastcode)
+        last_codes_lst.insert(0, code)
+        last_codes_lst.pop()
+        # print(lastcode)
+
+        comp = sum(last_codes_lst)
         if comp == 0:
             if not is_paused:
                 pause_button()
                 is_paused = True
+        # Trigger Play Command if code occurs exactly once in list of last codes
         elif comp == code:
+            media_player.play_feedback_sound()
             is_paused = False
-            if comp ==last:
+            if comp == last_media_code:
                 pause_button()
                 print("resume")
             else:
-                last = code
-        
-                print("starting" +str(code))
-                
-
-       #     # button code 0 == pause
-        #    if code == 0:
-         #       if media_player.is_playing():
-          #          pause_button()
-           #         print("pausing")
-            #        media_player.play_feedback_sound()
-            #else:
-             
-             #   print(code)
-
+                last_media_code = code
                 # Check if found code occurs in media list
-              #  for m in media_list:
-               #     if m[1] == code:
-                #        print("Playing " + m[0] + " at " + m[2])
-                 #       player = media_player.play_media(m[2])
+                for m in media_list:
+                    if m[1] == code:
+                        print("Playing " + m[0] + " at " + m[2])
+                    player = media_player.play_media(m[2])
+
+                print("starting" + str(code))
 
         time.sleep(SLEEP_DELAY)
 
 
 def read_tag(reader):
-    
     if reader.read_id_no_block():
         return reader.read_id()
     else:
         return 0
-
 
 
 media_list = media.media_list.list
