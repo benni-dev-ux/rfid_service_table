1311066621694886fd412f65cacfaa348de5dd7b
Benedikt <benediktsteinmueller@googlemail.com>
2022-01-15 18:44:24 +0100

    slow progress

--- rc522_reader.py
+++ rc522_reader.py
@@ -12,7 +12,7 @@ from screen_toggle import *
 SCREEN_TURN_OFF = False
 START_UP_SOUND = True
 FORCE_ANALOG_SOUND = False
-SLEEP_DELAY = 0.05
+SLEEP_DELAY = 0.1
 
 # Button Pins
 black_button = 19
@@ -53,12 +53,12 @@ def main():
     #     startup = pygame.mixer.Sound(
     #         "/home/pi/Raspi_RFID_player/assets/startup.wav")
     #     startup.play()
-    media_player.play_feedback_sound()
+    #media_player.play_feedback_sound()
     print("\n RFID Player Ready")
 
     global player
 
-    # GPIO   3, 4, 17 and 10
+
     button1 = Button(black_button, hold_time=2)
     button2 = Button(yellow_button, bounce_time=0.1)
     button3 = Button(blue_button, bounce_time=0.1)
@@ -72,34 +72,43 @@ def main():
 
     reader = SimpleMFRC522()
 
-    lastcode = -1
+    lastcode = [-1,-1,-1,-1,-1]
 
     # Main Loop of the App: Constantly checking for new  RFID input
     while True:
+        #print("reading")
         code = read_tag(reader)
+        #print(code)
 
-        if code is not lastcode:
+        if not code in lastcode:
             # button code 0 == pause
             if code == 0:
-                pause_button()
+                if media_player.is_playing():
+                    pause_button()
+                    print("pausing")
+                    media_player.play_feedback_sound()
             else:
-                media_player.play_feedback_sound()
+             
+                print(code)
 
                 # Check if found code occurs in media list
                 for m in media_list:
                     if m[1] == code:
                         print("Playing " + m[0] + " at " + m[2])
                         player = media_player.play_media(m[2])
-
+        lastcode.insert(0,code)
+        lastcode.pop()
+        print(lastcode)
         time.sleep(SLEEP_DELAY)
 
 
 def read_tag(reader):
-    tag_id = reader.read_id()
-    if tag_id == "":
-        return 0
+    
+    if reader.read_id_no_block():
+        return reader.read_id()
     else:
-        return tag_id
+        return 0
+
 
 
 media_list = media.media_list.list
