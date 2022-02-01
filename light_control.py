857175780fa30eae816020963057ca2a07987ed9
Benedikt <benediktsteinmueller@googlemail.com>
2022-02-01 23:42:00 +0100

    testing more light stuff

--- light_control.py
+++ light_control.py
@@ -3,18 +3,18 @@ import neopixel
 import time
 
 
-pixel_pin = board.D18
-num_pixels = 32
+gpio_pin = board.D18
+num_leds = 32
 ORDER = neopixel.RGB
 
 pixels = neopixel.NeoPixel(
-    pixel_pin, num_pixels, brightness=0.2, auto_write=False, pixel_order=ORDER
+    gpio_pin, num_leds, brightness=0.2, auto_write=False, pixel_order=ORDER
 )
 
 
-pixels.fill((255,0,0))
+def fill_light_ring(percentage, color):
+    filled = round((percentage * num_leds)/100)
 
-#for i in range(32):
-    
- #   pixels[i] = (0,255,0)
-  #  time.sleep(1)
+    for i in range(filled):
+
+        pixels[i] = (color)
