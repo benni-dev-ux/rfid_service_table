cb6df78a1589e1a044fd9c8467896b1f2d938b6c
Benedikt <benediktsteinmueller@googlemail.com>
2022-02-02 00:01:41 +0100

    fixed lighting controls

--- light_control.py
+++ light_control.py
@@ -5,7 +5,7 @@ import time
 
 gpio_pin = board.D18
 num_leds = 32
-ORDER = neopixel.RGB
+ORDER = neopixel.GRB
 
 pixels = neopixel.NeoPixel(
     gpio_pin, num_leds, brightness=0.2, auto_write=False, pixel_order=ORDER
@@ -18,3 +18,16 @@ def fill_light_ring(percentage, color):
     for i in range(filled):
 
         pixels[i] = (color)
+        time.sleep(0.1)
+        pixels.show()
+
+
+def turn_off_lights():
+    pixels.fill((0,0,0))
+    pixels.show()
+    
+fill_light_ring(100,(0,0,255))
+
+
+
+
