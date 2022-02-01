f8ed5a07e6da7a158f25d680eb5e5786db31cd67
Benedikt <benediktsteinmueller@googlemail.com>
2022-02-01 19:11:48 +0100

    added testcode

--- light_control.py
+++ light_control.py
@@ -1,5 +1,11 @@
 import board
 import neopixel
+import time
 
 
-pixels = neopixel.NeoPixel(board.D18, 30)
+pixels = neopixel.NeoPixel(board.D18, 32)
+
+for i in range(32):
+    
+    pixels[i] = (0,255,0)
+    time.sleep(1)
