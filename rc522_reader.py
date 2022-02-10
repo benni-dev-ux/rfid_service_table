4fc1dde5066f29c88a490e9126175cab582c13d4
Benedikt <benediktsteinmueller@googlemail.com>
2022-02-10 23:17:24 +0100

    added keyboardinterrupt handling

--- rc522_reader.py
+++ rc522_reader.py
@@ -1,4 +1,5 @@
 import time
+import sys
 from subprocess import check_call
 
 from gpiozero import Button
@@ -59,70 +60,74 @@ def placeholder_button():
 
 
 def main():
-    if START_UP_SOUND:
-        media_player.play_sound_effect("startup.wav")
-    print("\n RFID Player Ready")
+    try:
+        if START_UP_SOUND:
+            media_player.play_sound_effect("startup.wav")
+        print("\n RFID Player Ready")
 
-    # Light Up the Table
-    if TABLE_RINGLIGHT:
-        light_control.fill_light_ring(
-            LIGHTRING_PERCENTAGE, LIGHT_COLOR)
+        # Light Up the Table
+        if TABLE_RINGLIGHT:
+            light_control.fill_light_ring(
+                LIGHTRING_PERCENTAGE, LIGHT_COLOR)
 
-    global player
+        global player
 
-    # Testvideo
-    media_list = []
-    test_video = "Testvideo", 524887201261, "/home/pi/rfid_service_table/assets/testvideo.mp4"
-    media_list.append(test_video)
+        # Testvideo
+        media_list = []
+        test_video = "Testvideo", 524887201261, "/home/pi/rfid_service_table/assets/testvideo.mp4"
+        media_list.append(test_video)
 
-    # media_list = media.media_list.list
+        # media_list = media.media_list.list
 
-    button1 = Button(black_button, hold_time=2)
-    button2 = Button(green_button, bounce_time=0.1)
-    button3 = Button(blue_button, bounce_time=0.1)
-    button4 = Button(yellow_button, bounce_time=0.1)
+        button1 = Button(black_button, hold_time=2)
+        button2 = Button(green_button, bounce_time=0.1)
+        button3 = Button(blue_button, bounce_time=0.1)
+        button4 = Button(yellow_button, bounce_time=0.1)
 
-    # Mapping functions to button presses
-    button1.when_pressed = power_button
-    button2.when_pressed = stop_button
-    button3.when_pressed = play_pause_button
-    button4.when_pressed = placeholder_button
+        # Mapping functions to button presses
+        button1.when_pressed = power_button
+        button2.when_pressed = stop_button
+        button3.when_pressed = play_pause_button
+        button4.when_pressed = placeholder_button
 
-    reader = SimpleMFRC522()
+        reader = SimpleMFRC522()
 
-    last_codes_lst = [-1, -1, -1, -1, -1]
-    last_media_code = -1
-    is_paused = False
+        last_codes_lst = [-1, -1, -1, -1, -1]
+        last_media_code = -1
+        is_paused = False
 
-    # Main Loop of the App: Constantly checking for new  RFID input
-    while True:
-        code = read_tag(reader)
-        last_codes_lst.insert(0, code)
-        last_codes_lst.pop()
+        # Main Loop of the App: Constantly checking for new  RFID input
+        while True:
+            code = read_tag(reader)
+            last_codes_lst.insert(0, code)
+            last_codes_lst.pop()
 
-        comp = sum(last_codes_lst)
-        if comp == 0:  # if sum of last 5 codes =0 -> Pause Media
-            if not is_paused:
-                play_pause_button()
-                is_paused = True
+            comp = sum(last_codes_lst)
+            if comp == 0:  # if sum of last 5 codes =0 -> Pause Media
+                if not is_paused:
+                    play_pause_button()
+                    is_paused = True
 
-        elif comp == code:  # Trigger Play Command if code occurs exactly once in list of last codes
-            media_player.play_sound_effect("beep.mp3")
-            is_paused = False
-            if comp == last_media_code:
-                play_pause_button()
-                print("resuming")
-            else:
-                last_media_code = code
-                # Check if found code occurs in media list
-                for m in media_list:
-                    if m[1] == code:
-                        print("Playing " + m[0] + " at " + m[2])
-                    player = media_player.play_media(m[2])
+            elif comp == code:  # Trigger Play Command if code occurs exactly once in list of last codes
+                media_player.play_sound_effect("beep.mp3")
+                is_paused = False
+                if comp == last_media_code:
+                    play_pause_button()
+                    print("resuming")
+                else:
+                    last_media_code = code
+                    # Check if found code occurs in media list
+                    for m in media_list:
+                        if m[1] == code:
+                            print("Playing " + m[0] + " at " + m[2])
+                        player = media_player.play_media(m[2])
 
-                print("starting" + str(code))
+                    print("starting" + str(code))
 
-        time.sleep(SLEEP_DELAY)  # resume after delay
+            time.sleep(SLEEP_DELAY)  # resume after delay
+    except KeyboardInterrupt:
+            light_control.turn_off_lights()
+            sys.exit()
 
 
 def read_tag(reader):
