900ebe6d8e76e73a630ff1f919f21f90e85defdf
Benedikt <benediktsteinmueller@googlemail.com>
2022-02-25 18:33:28 +0100

    added new test tags

--- media/media_list.py
+++ media/media_list.py
@@ -1,5 +1,7 @@
 # Name, TAG ID and Filename
 m1 = ["Testvideo", 524887201261, "testvideo.mp4"]
 m2 = ["Testaudio", 252006438210, "testaudio.mp3"]
+m3 = ["Testvideo", 180448400812, "testvideo.mp4"]
+m4 = ["Testaudio", 800769250639, "testaudio.mp3"]
 
-list = [m1, m2]
+list = [m1, m2,m3,m4]
