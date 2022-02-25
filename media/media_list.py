67e6ccf525846e10bb5ba91e5f6eb36450d9963a
Benedikt <benediktsteinmueller@googlemail.com>
2022-02-25 17:05:10 +0100

    fixed comment

--- media/media_list.py
+++ media/media_list.py
@@ -1,6 +1,4 @@
-# List that maps tags to respective filepaths
-# First three numbers are to be left out from every Tag ID
-
+# File name (not relevant), TAG ID and Filename
 m1 = ["Testvideo", 524887201261, "testvideo.mp4"]
 m2 = ["Testaudio", 252006438210, "testaudio.mp3"]
 
