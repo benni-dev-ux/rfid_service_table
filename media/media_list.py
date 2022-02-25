3b0d9bc95782785dfbd15b7ac1d07f1e3667a5e3
Benedikt <benediktsteinmueller@googlemail.com>
2022-02-25 17:04:06 +0100

    removed media folder from gitignore

---
+++ media/media_list.py
@@ -0,0 +1,7 @@
+# List that maps tags to respective filepaths
+# First three numbers are to be left out from every Tag ID
+
+m1 = ["Testvideo", 524887201261, "testvideo.mp4"]
+m2 = ["Testaudio", 252006438210, "testaudio.mp3"]
+
+list = [m1, m2]
