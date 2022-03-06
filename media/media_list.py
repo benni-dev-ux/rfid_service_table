5b3de46e469ab1397fda747467c91a468cb0b47e
Benedikt <benediktsteinmueller@googlemail.com>
2022-03-06 20:18:51 +0100

    added tags to list

--- media/media_list.py
+++ media/media_list.py
@@ -4,4 +4,22 @@ m2 = ["Testaudio", 252006438210, "testaudio.mp3"]
 m3 = ["Testvideo", 180448400812, "testvideo.mp4"]
 m4 = ["Testaudio", 800769250639, "testaudio.mp3"]
 
-list = [m1, m2,m3,m4]
+m5 = ["1", 584197706746, "testvideo.mp4"]
+m6 = ["2", 584193970609, "testvideo.mp4"]
+m7 = ["3", 584195740122, "testvideo.mp4"]
+m8 = ["4", 584194494889, "testvideo.mp4"]
+m9 = ["5", 584196723400, "testvideo.mp4"]
+m10 = ["6", 584190430836, "testvideo.mp4"]
+m11 = ["7", 584189906540, "testvideo.mp4"]
+m12 = ["8", 584196199120, "testvideo.mp4"]
+m13 = ["9", 584190955132, "testvideo.mp4"]
+m14 = ["10", 584191414150, "testvideo.mp4"]
+m15 = ["11", 584195084704, "testvideo.mp4"]
+m16 = ["12", 584197182402, "testvideo.mp4"]
+m17 = ["13", 584193052573, "testvideo.mp4"]
+m18 = ["14", 584193511609, "testvideo.mp4"]
+m19 = ["15", 584192003981, "testvideo.mp4"]
+
+
+
+list = [m1, m2,m3,m4,m4,m5,m6,m7,m8,m9,m10,m11,m12,m13,m14,m15,m16,m17,m18,m19]
